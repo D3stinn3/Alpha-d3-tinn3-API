b43641b0d7158135fa571cf52602019772ab54d1
D3stinn3 <paynedestinne@gmail.com>
2023-03-20 11:09:08 +0300

    Views

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,6 +1,14 @@
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
 
+
 # Create your views here.
 def api_nyumbani(request, *args, **kwargs):
+    body = request.body
+    try:
+        data = json.loads
+    except:
+        pass
+    print(data.keys)
     return JsonResponse({"message": "Hi there!"})
