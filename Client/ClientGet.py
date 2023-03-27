59eea50692663dd8dbc997f84e8ea77ef9b09e19
D3stinn3 <paynedestinne@gmail.com>
2023-03-27 12:32:43 +0300

    Setting Changes

--- Client/ClientGet.py
+++ Client/ClientGet.py
@@ -2,7 +2,7 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api/"
 
-Responsee = requests.post(endpoint, json={"title":"Hello World", "content": "Paka na Panya", "context": "Form 2", "price": 3000})
+Responsee = requests.post(endpoint, json={"title":"Maisha", "content": "Njia Panda", "context": "Grade 2", "price": 3000})
 
 # print(Responsee.status_code)
 json_ = Responsee.json()
